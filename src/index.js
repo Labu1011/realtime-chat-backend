40034cd1f5f09749de5cf0a577a908eca1453bd4
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2025-02-24 23:54:36 +0600

    Create auth routes and connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
 
-const app = express()
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001")
-})
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+
+  connectDB();
+});

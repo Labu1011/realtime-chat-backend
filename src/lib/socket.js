561cb96d9ca7e775211d065aee3be0ae0defa065
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2025-03-13 20:22:14 +0600

    Add online users detection system on socket

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -11,11 +11,21 @@ const io = new Server(server, {
   },
 });
 
+// used to store online users
+const socketMap = {}; // {userId: socketId}
+
 io.on("connection", (socket) => {
   console.log("A user connected: ", socket.id);
 
+  const userId = socket.handshake.query.userId;
+  if (userId) socketMap[userId] = socket.id;
+
+  io.emit("getOnlineUsers", Object.keys(socketMap));
+
   socket.on("disconnect", () => {
     console.log("A user disconnected: ", socket.id);
+    delete socketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(socketMap));
   });
 });
 

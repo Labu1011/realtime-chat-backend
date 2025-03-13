d7e63b00391f4546bf599906eca83b0c9fa403e1
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2025-03-13 21:19:53 +0600

    Add realtime chat functionality

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -11,6 +11,10 @@ const io = new Server(server, {
   },
 });
 
+export function getReceiverSocketId(userId) {
+  return socketMap[userId];
+}
+
 // used to store online users
 const socketMap = {}; // {userId: socketId}
 
